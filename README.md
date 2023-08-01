898c77b5bc55dd2e4d5fe2588c8dd3ddd4d53935
junohm410 <junohm410@gmail.com>
2023-08-01 23:06:54 +0900

    メモアプリ用のファイルを作成・追加
    メモアプリ memo_app.rb ほか、ビューファイルなど関連するファイルを作成・追加し、READMEを更新。

---
+++ README.md
@@ -0,0 +1,22 @@
+##  概要
+Sinatraを使って作成したメモアプリです。
+タイトル付きのメモの作成・編集・削除が可能です。
+
+## インストール
+1. `git clone`を実行してローカルに複製。
+```
+$ git clone https://github.com/junohm410/sinatra-practice-memoapp.git
+```
+
+2. `bundle install`を実行。
+```
+$ bundle install
+```
+
+## 実行
+1. `app.rb`を実行。
+```
+$ bundle exec ruby memo_app.rb
+```
+
+2. ブラウザで`http://localhost:4567/memos`を開く。
---
+++ memo_app.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'cgi'
+require 'json'
+require 'securerandom'
+
+enable :method_override
+
+helpers do
+  def h(text)
+    CGI.escape_html(text)
+  end
+end
+
+def create_memos_file
+  File.open('memo_data/memos.json', 'w') do |file|
+    file.puts '[]'
+  end
+end
+
+def read_memos_file
+  File.open('memo_data/memos.json') do |file|
+    JSON.parse(file.read, symbolize_names: true)
+  end
+end
+
+def create_new_memo(title, text)
+  memo_hash = {}
+  memo_hash[:id] = SecureRandom.uuid
+  memo_hash[:title] = title
+  memo_hash[:text] = text
+  memo_hash
+end
+
+def update_memo_file(memos)
+  File.open('memo_data/memos.json', 'w') do |file|
+    JSON.dump(memos, file)
+  end
+end
+
+get '/' do
+  redirect to('/memos')
+end
+
+get '/memos' do
+  @title = 'メモ一覧'
+  create_memos_file unless File.exist?('memo_data/memos.json')
+  @memos = read_memos_file
+  erb :memos
+end
+
+post '/memos' do
+  memos = read_memos_file
+  new_memo = create_new_memo(params[:title], params[:content])
+  memos << new_memo
+  update_memo_file(memos)
+  redirect to('/memos')
+end
+
+get '/memos/new' do
+  @title = 'メモ作成'
+  erb :new
+end
+
+get '/memos/:id' do |id|
+  @title = 'メモ詳細'
+  @id = id
+  target_memo = read_memos_file.find { |memo| memo[:id] == id }
+  target_memo.nil? ? pass : @memo = target_memo
+  erb :memo
+end
+
+patch '/memos/:id' do |id|
+  memos = read_memos_file
+  target_memo = memos.find { |memo| memo[:id] == id }
+  target_memo[:title] = params[:title]
+  target_memo[:text] = params[:content]
+  update_memo_file(memos)
+  redirect to('/memos')
+end
+
+delete '/memos/:id' do |id|
+  memos = read_memos_file
+  target_memo = memos.find { |memo| memo[:id] == id }
+  memos.delete(target_memo)
+  update_memo_file(memos)
+  redirect to('/memos')
+end
+
+get '/memos/:id/edit' do |id|
+  @title = 'メモ編集'
+  @id = id
+  @memo = read_memos_file.find { |memo| memo[:id] == id }
+  erb :edit
+end
+
+get '/memos/*' do
+  status 404
+end
+
+not_found do
+  'ファイルが存在しません'
+end
