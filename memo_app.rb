a1a95abf7f2c851cdb181eea3e71cc813389741f
junohm410 <junohm410@gmail.com>
2023-08-09 18:12:51 +0900

    コメントと、jsonライブラリを削除

--- memo_app.rb
+++ memo_app.rb
@@ -3,7 +3,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'cgi'
-require 'json'
 require 'securerandom'
 require 'pg'
 
@@ -15,32 +14,6 @@ helpers do
   end
 end
 
-# def create_memos_file
-#   File.open('memo_data/memos.json', 'w') do |file|
-#     file.puts '[]'
-#   end
-# end
-
-# def read_memos_file
-#   File.open('memo_data/memos.json') do |file|
-#     JSON.parse(file.read, symbolize_names: true)
-#   end
-# end
-
-# def create_new_memo(title, text)
-#   {
-#     id: SecureRandom.uuid,
-#     title:,
-#     text:
-#   }
-# end
-
-# def update_memo_file(memos)
-#   File.open('memo_data/memos.json', 'w') do |file|
-#     JSON.dump(memos, file)
-#   end
-# end
-
 def symbolize_keys(hash)
   hash.transform_keys(&:to_sym)
 end
@@ -49,14 +22,6 @@ get '/' do
   redirect to('/memos')
 end
 
-# get '/memos' do
-#   @title = 'メモ一覧'
-#   create_memos_file unless File.exist?('memo_data/memos.json')
-#   @memos = read_memos_file
-#   erb :memos
-# end
-
-#DB版
 get '/memos' do
   @title = 'メモ一覧'
 
@@ -65,20 +30,10 @@ get '/memos' do
     conn.exec( "SELECT * FROM memo ORDER BY added_time ASC" ) do |result|
       result.map { |row| symbolize_keys(row) }
     end
-  # @memos = memos.map { |memo| hash_symbolize_keys(memo) }
   conn.finish
   erb :memos
 end
 
-# post '/memos' do
-#   memos = read_memos_file
-#   new_memo = create_new_memo(params[:title], params[:content])
-#   memos << new_memo
-#   update_memo_file(memos)
-#   redirect to('/memos')
-# end
-
-#DB版
 post '/memos' do
   id = SecureRandom.uuid
   conn = PG.connect( dbname: 'memo' )
@@ -92,25 +47,11 @@ get '/memos/new' do
   erb :new
 end
 
-# get '/memos/:id' do |id|
-#   @title = 'メモ詳細'
-#   @id = id
-#   target_memo = read_memos_file.find { |memo| memo[:id] == id }
-#   raise Sinatra::NotFound if target_memo.nil?
-
-#   @memo = target_memo
-#   erb :memo
-# end
-
-# DB版
 get '/memos/:id' do |id|
   @title = 'メモ詳細'
   @id = id
 
   conn = PG.connect( dbname: 'memo' )
-  # conn.exec_params( "SELECT * FROM memo WHERE id = $1", [id] ) do |result|
-  #   target_memo = result.map { |row| row }
-  # end
   search_result = conn.exec_params( "SELECT * FROM memo WHERE id = $1", [id] )
   raise Sinatra::NotFound if search_result.ntuples.zero?
   
@@ -119,16 +60,6 @@ get '/memos/:id' do |id|
   erb :memo
 end
 
-# patch '/memos/:id' do |id|
-#   memos = read_memos_file
-#   target_memo = memos.find { |memo| memo[:id] == id }
-#   target_memo[:title] = params[:title]
-#   target_memo[:text] = params[:content]
-#   update_memo_file(memos)
-#   redirect to('/memos')
-# end
-
-#DB版
 patch '/memos/:id' do |id|
   conn = PG.connect( dbname: 'memo' )
   conn.exec_params( "UPDATE memo SET title = $1, text = $2 WHERE id = $3", [params[:title], params[:content], id] )
@@ -136,14 +67,6 @@ patch '/memos/:id' do |id|
   redirect to('/memos')
 end
 
-# delete '/memos/:id' do |id|
-#   memos = read_memos_file
-#   memos.delete_if { |memo| memo[:id] == id }
-#   update_memo_file(memos)
-#   redirect to('/memos')
-# end
-
-#DB版
 delete '/memos/:id' do |id|
   conn = PG.connect( dbname: 'memo' )
   conn.exec_params( "DELETE FROM memo WHERE id = $1", [id] )
@@ -151,22 +74,11 @@ delete '/memos/:id' do |id|
   redirect to('/memos')
 end
 
-# get '/memos/:id/edit' do |id|
-#   @title = 'メモ編集'
-#   @id = id
-#   @memo = read_memos_file.find { |memo| memo[:id] == id }
-#   erb :edit
-# end
-
-#DB版
 get '/memos/:id/edit' do |id|
   @title = 'メモ編集'
   @id = id
 
   conn = PG.connect( dbname: 'memo' )
-  # conn.exec_params( "SELECT * FROM memo WHERE id = $1", [id] ) do |result|
-  #   target_memo = result.map { |row| row }
-  # end
   search_result = conn.exec_params( "SELECT * FROM memo WHERE id = $1", [id] )
   raise Sinatra::NotFound if search_result.ntuples.zero?
   
